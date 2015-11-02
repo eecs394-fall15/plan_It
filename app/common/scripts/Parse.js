578a5730b80783570d0616988e9cee6c904a1ffe
Jaiveer Kothari <jaiveerkothari@yahoo.com>
2015-11-01 20:28:35 -0600

    initialized parse

---
+++ app/common/scripts/Parse.js
@@ -0,0 +1,6 @@
+angular
+  .module('common')
+  .factory('Parse', function () {
+  	Parse.initialize("s0bBxGQmofmKGs4hKvgURP4nDYQJmNFblcRUXXh2", "GKf1pBCOMnq8GVc9WuEcEuzSBMgnSU5NesxtnqCB","DTUPsbEoPmgeYlIYo7tnMuuWrclCwlTxlmRjjP4a");
+    return Parse;
+});
