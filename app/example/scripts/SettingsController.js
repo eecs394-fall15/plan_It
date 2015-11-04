2faae6c87bc5cce0845e9b4a403594cbf46c02a1
sophialou <sophialou@users.noreply.github.com>
2015-11-03 20:32:01 -0600

    working title from database

--- app/example/scripts/SettingsController.js
+++ app/example/scripts/SettingsController.js
@@ -2,34 +2,24 @@ angular
   .module('example')
   .controller('SettingsController', function($scope, supersonic) {
     $scope.navbarTitle = "Settings";
-    $scope.itenary=null;
-
-
- function f1 () {
- 	supersonic.logger.log("f1 called");
- 	var Itenary = Parse.Object.extend("Itenary");
-	var query = new Parse.Query(Itenary);
-	query.equalTo("objectId", "AzXTi8GA53");
-	query.find({
-	  success: function(results) {
-	    supersonic.logger.log("queried successfully");
-	    // Do something with the returned Parse.Object values
-	    $scope.$apply( function () {
-          $scope.itenary = results[0];
-          supersonic.logger.log(results);
-          supersonic.ui.dialog.alert(results);
-
-        });
-	    
-	  },
-	  error: function(error) {
-	    supersonic.logger.log("query failed");
-	  }
-	});
-
- 	// body...
- }
     
- f1();
-
+    supersonic.ui.views.current.whenVisible(function() {
+        supersonic.logger.log("f1 called");
+        var Itenary = Parse.Object.extend("Itenary");
+        var query = new Parse.Query(Itenary);
+        query.equalTo("objectId", "AzXTi8GA53");
+        query.find({
+            success: function(results) {
+            supersonic.logger.log("queried successfully");
+            // Do something with the returned Parse.Object values
+                $scope.itenary = results[0];
+                supersonic.logger.log(results);
+     },
+        error: function(error) {
+        supersonic.logger.log("query failed");
+        }  
+        });
+        
+        
+ });
   });
