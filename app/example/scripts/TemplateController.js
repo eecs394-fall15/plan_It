1689e14cd873170944183c2bebd18189d4f9ef1d
sophialou <sophialou@users.noreply.github.com>
2015-11-05 21:12:19 -0600

    Saves template successfully to database
    Additional code to save events to database, right now buggy

--- app/example/scripts/TemplateController.js
+++ app/example/scripts/TemplateController.js
@@ -21,6 +21,43 @@ angular
     $scope.submitForm = function(){
           supersonic.logger.log($scope.choices);
           supersonic.logger.log($scope.template); 
-    }
+        var Itenerary = Parse.Object.extend("Itenary"); 
+        var itenerary = new Itenerary(); 
+        
+        itenerary.set("title",$scope.template.city); 
+        itenerary.set("country",$scope.template.country);
+        itenerary.set("date",$scope.template.date); 
+        
+        itenerary.save(null, {
+            success: function(itenerary) {
+                supersonic.logger.log("itenerary successfully saved!!!!");
+                
+                for (var i = 0; i < $scope.choices.length;i++){
+                    var eventinfo = $scope.choices[i];
+                    
+                    var Event = Parse.Object.extend("Event"); 
+                    var event = new Event(); 
+                    
+                    event.set("title",eventinfo.title);
+                    event.set("parItenerary",itenerary); 
+                    event.set("time",eventinfo.time);
+                    event.set("info",eventinfo.info); 
+                    
+                    event.save(null, {
+                        success: function(event) {
+                            supersonic.logger.log("event successfully saved");
+                        },
+                        error: function(event, error){
+                            supersonic.logger.log("event failed to save");
+                        }
+                    });
+                }
+                
+            },
+            error: function(itenerary, error) {
+                supersonic.logger.log("itenerary failed to save");    
+            }
+        }); 
+    };
 
   });
