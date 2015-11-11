4a298d1be48dc498473d626827fa43424b905565
sophialou <sophialou@users.noreply.github.com>
2015-11-10 21:49:56 -0600

    from db up to suggestions

--- app/example/scripts/customizeController.js
+++ app/example/scripts/customizeController.js
@@ -1,52 +1,16 @@
 angular
   .module('example')
   .controller('customizeController', function($scope, supersonic) {
-    $scope.navbarTitle = "Customize";
-    $scope.events = []; 
-    $scope.suggestions = {};
-    $scope.eventswsugs=[];
-    
-    $scope.findSuggestions = function(eventId) {
-        supersonic.logger.info(eventId); 
-        supersonic.logger.log("finding sugs"); 
-        var Suggestion = Parse.Object.extend("Suggestion"); 
-        var suggestionQuery = new Parse.Query(Suggestion); 
-        suggestionQuery.equalTo("parEventId", eventId);
-        suggestionQuery.find({
-            success: function(allsuggestions){
-                supersonic.logger.log(allsuggestions);
-                for(var j=0; j< $scope.eventswsugs.length;j++){
-                    if($scope.eventswsugs[j].events_id == eventId){
-                        
-                        $scope.eventswsugs[j].events_sug=allsuggestions;
-                        
-                    }
-                }
-                
-                //$scope.eventswsugs.push({events_id:eventId,events_sug:allsuggestions})
-                
-//                
-//                for (var i = 0; i < $scope.events.length;i++){
-//                    var e = $scope.events[i];
-//                    if (e.id == eventId) {
-//                        e.suggestions = allsuggestions; 
-//                          supersonic.logger.log(e);
-//                    }
-//                }
-//           
-                
-            },
-            error: function(error){
-                
-            }
-        });
-    };
+  $scope.events = []; 
+
     
     supersonic.ui.views.current.whenVisible(function() {
         supersonic.logger.log("f1 called");
+        
         var Itenary = Parse.Object.extend("Itenary");
         var query = new Parse.Query(Itenary);
         var iternaryIdstr="AzXTi8GA53";
+        
         query.equalTo("objectId", iternaryIdstr);
         query.find({
             success: function(results) {
@@ -57,21 +21,45 @@ angular
                 $scope.itenary = results[0];
                 supersonic.logger.log(results);
                 
+                
                 var Event = Parse.Object.extend("Event"); 
                 var eventQuery = new Parse.Query(Event); 
-                eventQuery.equalTo("parIter", iternaryIdstr); 
+                eventQuery.equalTo("parIter", $scope.itenary.id); 
                 eventQuery.find({
                     success: function(resultsE) {
-                        for (var i=0; i<resultsE.length;i++){
-                            $scope.eventswsugs.push({events_id:resultsE[i],events_sug:[]});
-                        }
+                        var tempEvents = resultsE; 
+                        var eventId = resultsE[2].id;
+                        var breakfastEvent = tempEvents[2]; // object
                         
-                        $scope.events = resultsE; 
+                        resultsE[0].suggs = []; // not making a difference 
+                        resultsE[1].suggs = []; 
+                        
+                        var Suggestion = Parse.Object.extend("Suggestion"); 
+                        var suggestionQuery = new Parse.Query(Suggestion); 
+                        suggestionQuery.equalTo("parEventId", eventId);
+                        suggestionQuery.find({
+                            success: function(resultsSugg) {
+                                
+                                breakfastEvent.suggs = resultsSugg; 
+                                
+                                $scope.tree = breakfastEvent.suggs[0].get('title');
+                                 $scope.tree2 = breakfastEvent.suggs[1].get('title');
+                                
+                               // $scope.tree = resultsSugg[0].get('title'); // yay 
+                            },
+                            error: function(error) { 
+                                  supersonic.logger.log(error); 
+                            }
+                          
+                        }); 
+                        
+                        $scope.events = tempEvents;
                         supersonic.logger.log(resultsE);
                         supersonic.logger.info(resultsE);    
 
                     },
                     error: function(error) {
+                         supersonic.logger.log(error); 
                     }
                 });
                         
@@ -80,7 +68,5 @@ angular
         supersonic.logger.log("query failed");
         }  
         });
-        
-        
  });
   });
