823483bbddc99fd73dbbc7d2a90837c5cc1875d5
Jaiveer Kothari <jaiveerkothari@yahoo.com>
2015-11-14 17:55:15 -0600

    save suggestions in customize ctrler

--- app/example/scripts/customizeController.js
+++ app/example/scripts/customizeController.js
@@ -34,7 +34,61 @@ angular
     });
 
     $scope.saveSuggestion = function  (suggestionId) {
+      var Suggestions = Parse.Object.extend("Suggestions");
+      var suggestions = new Suggestions();
+      $scope.sug_isSaved=false;
       
+      
+      var query = new Parse.Query(Suggestions);
+
+      query.equalTo("objectId", suggestionId);
+
+
+
+      query.find({
+            success: function(sug) {
+            supersonic.logger.log("found the sug"); 
+            supersonic.logger.log(sug); 
+
+            sug[0].set("isSaved",true);
+            supersonic.logger.log("set isSaved");
+            sug[0].save(null, {
+              success: function(sug1) {
+                // Now let's update it with some new data. In this case, only cheatMode and score
+                // will get sent to the cloud. playerName hasn't changed.
+                supersonic.logger.log("sug saved ");
+              }
+            });
+                
+                
+                        
+      },
+        error: function(error) {
+        supersonic.logger.log("query fsdfba failed");
+        }  
+      });
+
+      
+      // suggestions.id= suggestionId;
+
+      // suggestions.set("isSaved",$scope.sug_isSaved);
+
+      // suggestions.save(null, {
+      //   success: function(suggestions) {
+
+      //     supersonic.logger.log("updated Suggestion");   
+      //     // Saved successfully.
+      //   },
+      //   error: function(suggestions, error) {
+      //     // The save failed.
+      //     // error is a Parse.Error with an error code and description.
+      //   }
+      // });
+
+
+
+
+
       // body...
     }
 
