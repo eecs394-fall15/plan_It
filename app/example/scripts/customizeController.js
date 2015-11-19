01ab5b8fba2a520ae79cb3f713f57423b8f46ebd
sophialou <sophialou@users.noreply.github.com>
2015-11-18 18:04:27 -0600

    Save, edit functionality
    - Replaced checkbox and button with just checkbox icon
    - Change color of "choice" suggestion

--- app/example/scripts/customizeController.js
+++ app/example/scripts/customizeController.js
@@ -2,15 +2,24 @@ angular
   .module('example')
   .controller('customizeController', function($scope, supersonic) {
 
+//    $scope.listLimit = 2;
+    
+   // $scope.limits =[]; // for each event
+   
     $scope.events = [];
     var itineraryId = null;
-    $scope.listLimit = 2;          
+    
+   /* $scope.getListLimit = function(current){
+        return limits[current];
+    }; */
+              
     supersonic.ui.views.current.params.onValue(function(itinerary_id){
                                                itineraryId =itinerary_id.id;
                                                });
 
     
     supersonic.ui.views.current.whenVisible(function() {
+        
         supersonic.logger.log("f1 called");
         
         var Itenary = Parse.Object.extend("Itinerary");
@@ -20,14 +29,15 @@ angular
         query.equalTo("objectId", itineraryId);
         query.include("events");
         query.include("events.suggestions");
-        //query.include("events.suggestions.tips"); 
+       // query.include("events.suggestions.tips"); 
+       // query.include("events.suggestions.tips.author"); 
         query.find({
             success: function(itinerary) {
             supersonic.logger.log("queried successfully");   
                 
                 $scope.itenary = itinerary[0];
-                supersonic.logger.log(itinerary);
-                 supersonic.logger.info(itinerary);
+            //    supersonic.logger.log(itinerary);
+            //     supersonic.logger.info(itinerary);
                  var eventsObj=itinerary[0].get("events")
                      
                   $scope.events = eventsObj.sort(function(a,b){
@@ -35,7 +45,12 @@ angular
                      b = new Date(b.get('time')); 
                       return a<b ? -1 : a>b ? 1 : 0;
                  });
-                        
+            var all=[];
+                for (var i =0;i< $scope.events.length;i++){
+                    all.push(2);
+                }
+                
+                 $scope.limits = all;       
      },
         error: function(error) {
         supersonic.logger.log("query failed");
@@ -47,14 +62,10 @@ angular
     $scope.saveSuggestion = function  (suggestionId) {
       var Suggestions = Parse.Object.extend("Suggestions");
       var suggestions = new Suggestions();
-      $scope.sug_isSaved=false;
-      
       
       var query = new Parse.Query(Suggestions);
 
       query.equalTo("objectId", suggestionId);
-
-
 
       query.find({
             success: function(sug) {
@@ -78,29 +89,33 @@ angular
         supersonic.logger.log("query fsdfba failed");
         }  
       });
-
-      
-      // suggestions.id= suggestionId;
-
-      // suggestions.set("isSaved",$scope.sug_isSaved);
-
-      // suggestions.save(null, {
-      //   success: function(suggestions) {
-
-      //     supersonic.logger.log("updated Suggestion");   
-      //     // Saved successfully.
-      //   },
-      //   error: function(suggestions, error) {
-      //     // The save failed.
-      //     // error is a Parse.Error with an error code and description.
-      //   }
-      // });
-
-
-
-
-
-      // body...
     }
+    
+    $scope.clearSuggestion = function  (suggestionId) {
+      var Suggestions = Parse.Object.extend("Suggestions");
+      var suggestions = new Suggestions();
+      
+      
+      var query = new Parse.Query(Suggestions);
 
+      query.equalTo("objectId", suggestionId);
+
+      query.find({
+            success: function(sug) {
+        //    supersonic.logger.log("found the sug"); 
+          //  supersonic.logger.log(sug); 
+
+            sug[0].set("isSaved",false);
+        //    supersonic.logger.log("set isSaved");
+            sug[0].save(null, {
+              success: function(sug1) {
+          //      supersonic.logger.log("sug saved ");
+              }
+            });                
+      },
+        error: function(error) {
+        supersonic.logger.log("query fsdfba failed");
+        }  
+      });
+    }
 });
