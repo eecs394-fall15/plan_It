d0e37b6c85dc64733d31e687869741a8e5283d37
sophialou <sophialou@users.noreply.github.com>
2015-11-27 00:10:04 -0700

    added background

--- app/example/scripts/TemplateController.js
+++ app/example/scripts/TemplateController.js
@@ -3,12 +3,17 @@ angular
   .controller('TemplateController', function($scope, supersonic) {
    $scope.navbarTitle = "Create Template"; 
    $scope.template = {};
+    
+    //steroids.view.setBackgroundImage("/icons/backgroundTeal.png");
+    steroids.view.setBackgroundColor("#5cd6d6");
+    
     $scope.submitForm = function(){
         var Itenerary = Parse.Object.extend("Itinerary"); 
         var itenerary = new Itenerary(); 
         
         itenerary.set("title",$scope.template.city); 
         itenerary.set("author", Parse.User.current());
+        itenerary.set("events",[]); 
         // itenerary.set("country",$scope.template.country);
         // itenerary.set("date",$scope.template.date); 
         
@@ -16,7 +21,7 @@ angular
             success: function(itenerary) {
                 supersonic.logger.log("itenerary successfully saved!!!!");
          
-                var view = new supersonic.ui.View("example#newItinerary");
+                var view = new supersonic.ui.View("example#customize");
                 supersonic.ui.layers.push(view, {
                     params: {
                         id: itenerary.id
@@ -29,6 +34,10 @@ angular
                 supersonic.logger.log(error); 
             }
         }); 
-    };
+    }
+    
+    $scope.goBack = function(){
+        supersonic.ui.layers.pop();
+    }
 
   });
