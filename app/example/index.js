0b05847d9f71e84a1519e9ea1bbf4a113c7805ef
NikhilThagadur <nikhilthagadur@gmail.com>
2015-12-11 12:09:03 -0600

    index changes to unit test

--- app/example/index.js
+++ app/example/index.js
@@ -1,4 +1,27 @@
 angular.module('example', [
   // Declare here all AngularJS dependencies that are shared by the example module.
   'supersonic'
-]);
+])
+.service("ParseService", function($q, supersonic){
+this.find = function()
+{
+	var deferred= $q.defer();
+	
+	var query = new Parse.Query("Itinerary");
+        query.notEqualTo("author", Parse.User.current());
+        query.equalTo("published",true); 
+        query.include("author");
+        query.find({
+            success: function (results) {
+                supersonic.logger.log("feedcontroller successful query");
+		deferred.resolve(results);
+            
+    },
+    error: function (error) {
+        alert("Error: " + error.code + " " + error.message);
+	deferred.reject(error);
+    }
+});
+	return deferred.promise;
+}
+});
