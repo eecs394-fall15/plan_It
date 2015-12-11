62dfb091f9faafe31bd4687222b15bc704d8f572
NikhilThagadur <nikhilthagadur@gmail.com>
2015-12-11 12:04:24 -0600

    login changes

--- app/example/scripts/LoginController.js
+++ app/example/scripts/LoginController.js
@@ -6,7 +6,7 @@ angular
       steroids.view.setBackgroundImage("/icons/backgroundTeal.png");
      // steroids.view.setBackgroundColor("#5cd6d6");
     //  supersonic.ui.tabs.show();
-      
+      supersonic.logger.log($scope.scenario);
     if($rootScope.currentUser)
     {
       $scope.scenario = 'Logged in';
@@ -25,7 +25,7 @@ angular
         $scope.scenario = 'Logged in';
         $scope.$apply(); // Notify AngularJS to sync currentUser
         //var animation = supersonic.ui.animate("curlDown");
-        //supersonic.ui.initialView.dismiss(animation);
+       // supersonic.ui.initialView.dismiss(animation);
       },
       error: function(user, error) {
         alert("Unable to sign up:  " + error.code + " " + error.message);
@@ -40,7 +40,7 @@ angular
         $scope.scenario = 'Logged in';
         $scope.$apply();
         //var animation = supersonic.ui.animate("curlDown");
-        //supersonic.ui.initialView.dismiss(animation);
+       // supersonic.ui.initialView.dismiss(animation);
       },
       error: function(user, error) {
         alert("Unable to log in: " + error.code + " " + error.message);
